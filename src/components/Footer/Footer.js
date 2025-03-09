9391be78ab40a327e83f6f198d31beb09c49ecd2
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-03-09 18:59:40 +0530

    home

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { FaYoutube, FaSpotify, FaFacebook } from "react-icons/fa"; 
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
+    const location = useLocation(); 
+
   return (
     <footer className="bg-primary text-light py-3 mt-4" style={{ fontFamily: "'Poppins', sans-serif" }}>
       <Container variant='primary '>
         <Row className="text-center">
           <Col className="d-flex flex-row justify-content-between" >
             <div className="mb-2 fs-1">The Generics</div>
-            <div >
+            {location.pathname === "/home"&&(<div >
               <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer" className="text-light me-3">
                 <FaYoutube size={30} />
               </a>
@@ -19,7 +22,7 @@ const Footer = () => {
               <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="text-light">
                 <FaFacebook size={30} />
               </a>
-            </div>
+            </div>)}
           </Col>
         </Row>
       </Container>
